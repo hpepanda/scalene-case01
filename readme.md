7b5ea18ac34c568394db009e2647c4ee0bf4471f
Kateryna Kuiavska <kuyavskih@gmail.com>
2015-08-28 13:01:08 +0300

    hosts file added

--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@
 
 2.  Add instance public ip to /etc/ansible/hosts  
     `[case-0]`  
-    `{{assigned_ip}}`  
+    `{assigned_ip}`  
 
 
 3.  `ansible-playbook deploy-case-0.yml --private-key={{instance_name}}.pem -u ubuntu`
--- start-instance-private.sh
+++ start-instance-private.sh
@@ -5,11 +5,22 @@ NETID=bbf314de-c466-4d78-ad47-10a9de081508
 echo "Enter Instance Name"
 read INSTNAME
 
+export CASE_0_BIN=/home/out/case-0
+
+echo "Provide path to expenses.war (default $CASE_0_BIN):"
+read CASE_0_BIN_NEW
+
+if [[ $CASE_0_BIN_NEW ]]; then
+    CASE_0_BIN=$CASE_0_BIN_NEW
+fi
+
+echo "$CASE_0_BIN"
+
 # create rsa keypair for the Instance
 nova keypair-add $INSTNAME > $INSTNAME.pem
 
 # create and lauch new Instance from th image "Ubuntu Server 14.04.1 LTS (amd64 20150706) - Partner Image" and store info about instance in the file
-nova boot --flavor "101" --image "564be9dd-5a06-4a26-ba50-9453f972e483" --key_name "$INSTNAME" --security_groups "DemoHAProxyAppSec,tomcat-sg,default" $INSTNAME --nic net-id=$NETID > $INSTNAME.data
+nova boot --flavor "101" --image "564be9dd-5a06-4a26-ba50-9453f972e483" --key_name "$INSTNAME" --security_groups "DemoHAProxyAppSec,tomcat-sg,default" $INSTNAME --poll --user-data hosts.sh --nic net-id=$NETID > $INSTNAME.data
 
 # sec fix to use keyfile and disallow access to the file with all data about new instance
 chmod 400 $INSTNAME.pem
